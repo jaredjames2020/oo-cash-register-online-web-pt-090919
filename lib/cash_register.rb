ddf706486fbb4f6925902a060a291afbd88db38f
Jared S <jaredjames2020@gmail.com>
2019-10-09 23:17:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,5 +15,10 @@ class CashRegister
   def add_item(title, price, quantity=1)
     quantity.times {@total = @total + price} 
   end
+  
+  def apply_discount
+    @total = @total - @discount
+  
+  end
 
 end
